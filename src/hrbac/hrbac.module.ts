ec20365eb1fc555e5f024854d55e0bec3b7d3ea6
Mark Wecke <wecke@neoskop.de>
2019-05-06 11:10:41 +0200

    fix: export hrbak options

--- src/hrbac/hrbac.module.ts
+++ src/hrbac/hrbac.module.ts
@@ -66,7 +66,8 @@ export class HrbacModule {
                 { provide: PermissionManager, useFactory: permissionManagerFactory, inject: [ StaticPermissionManager, HRBAC_OPTIONS ] }
             ], exports: [
                 RoleManager,
-                PermissionManager
+                PermissionManager,
+                HRBAC_OPTIONS
             ]
         }
     }
@@ -81,6 +82,10 @@ export class HrbacModule {
                 { provide: RoleManager, useFactory: roleManagerFactory, inject: [ StaticRoleManager, HRBAC_OPTIONS ] },
                 { provide: PermissionManager, useFactory: permissionManagerFactory, inject: [ StaticPermissionManager, HRBAC_OPTIONS ] },
                 ...this.createAsyncProviders(options)
+            ], exports: [
+                RoleManager,
+                PermissionManager,
+                HRBAC_OPTIONS
             ]
         }
     }
