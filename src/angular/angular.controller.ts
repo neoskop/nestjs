82519743b26bc05d2ba3a42714848ddd18f44978
draco <wecke@neoskop.de>
2020-04-09 10:22:06 +0200

    fix: fix angular9 rendering

--- src/angular/angular.controller.ts
+++ src/angular/angular.controller.ts
@@ -3,7 +3,7 @@ import 'zone.js/dist/zone-node';
 
 import { Type } from '@angular/core';
 import { APP_BASE_HREF } from '@angular/common';
-import { renderModule } from '@angular/platform-server';
+import { renderModule, INITIAL_CONFIG } from '@angular/platform-server';
 import { Controller, Get, Next, Request, Response } from '@nestjs/common';
 import express from 'express';
 import fs from 'fs';
@@ -14,21 +14,26 @@ import { IAngularAppOptions } from './tokens';
 
 @Controller()
 export class AngularController<T extends IAngularAppOptions = IAngularAppOptions> {
-    protected readonly _static = this.mode === 'ssr' && this.options.www ? express.static(path.resolve(this.options.www), { index: false }) : null;
-    protected readonly _template = this.mode === 'ssr' && this.options.www ? fs.readFileSync(path.join(this.options.www, 'index.html'), 'utf-8') : null;
+    protected readonly _static =
+        this.mode === 'ssr' && this.options.www ? express.static(path.resolve(this.options.www), { index: false }) : null;
+    protected readonly _template =
+        this.mode === 'ssr' && this.options.www ? fs.readFileSync(path.join(this.options.www, 'index.html'), 'utf-8') : null;
     protected readonly _bundle = this.mode === 'ssr' && this.options.main ? loadBundle(this.options.main) : null;
-    protected readonly _proxy = this.mode === 'proxy' && this.options.target ? proxy({ target: this.options.target, changeOrigin: true, ws: true }) : null;
+    protected readonly _proxy =
+        this.mode === 'proxy' && this.options.target ? proxy({ target: this.options.target, changeOrigin: true, ws: true }) : null;
 
     protected readonly router = express.Router();
 
-    constructor(protected readonly mode: 'ssr' | 'proxy',
+    constructor(
+        protected readonly mode: 'ssr' | 'proxy',
         protected readonly options: T,
-        protected readonly nonceFactory?: (request : express.Request, response : express.Response) => string) {
+        protected readonly nonceFactory?: (request: express.Request, response: express.Response) => string
+    ) {
         this.init();
     }
 
     protected init() {
-        if((this._static && this._template && this._bundle) || this._proxy) {
+        if ((this._static && this._template && this._bundle) || this._proxy) {
             this.router.get('*.*', this.getStaticAssets.bind(this));
             this.router.get('*', this.getAngular.bind(this));
         }
@@ -47,33 +52,45 @@ export class AngularController<T extends IAngularAppOptions = IAngularAppOptions
     }
 
     protected _ssr(request: express.Request, response: express.Response, next: express.NextFunction) {
-        Zone.current.fork({
-            name: 'angular-ssr',
-            properties: { request, response }
-        }).run(async () => {
-            try {
-                const html = await this._bundle!.renderModule(this._bundle!.Module, {
-                    document: this._template!,
-                    url: `${request.protocol}://${request.get('host')}${request.url}`,
-                    extraProviders: [
-                        { provide: 'REQUEST', useValue: request },
-                        { provide: 'RESPONSE', useValue: response },
-                        { provide: APP_BASE_HREF, useValue: request.baseUrl },
-                        ...(this.options.providers || [])
-                    ]
-                });
+        Zone.current
+            .fork({
+                name: 'angular-ssr',
+                properties: { request, response }
+            })
+            .run(async () => {
+                try {
 
-                response.header('Content-Type', 'text/html');
-                if(this.nonceFactory) {
-                    response.end(html.replace(/<script(?: type="text\/javascript")?/g, `$& nonce="${this.nonceFactory(request, response)}"`));
-                } else {
-                    response.end(html);
+                    const html = await this._bundle!.renderModule(this._bundle!.Module, {
+                        // document: this._template!,
+                        // url: `${request.protocol}://${request.get('host')}${request.url}`,
+                        extraProviders: [
+                            ...(this.options.providers || []),
+                            { provide: 'REQUEST', useValue: request },
+                            { provide: 'RESPONSE', useValue: response },
+                            { provide: APP_BASE_HREF, useValue: request.baseUrl },
+                            {
+                                provide: INITIAL_CONFIG,
+                                useValue: {
+                                    doc: this._template!,
+                                    url: `${request.protocol}://${request.get('host')}${request.url}`
+                                }
+                            }
+                        ]
+                    });
+
+                    response.header('Content-Type', 'text/html');
+                    if (this.nonceFactory) {
+                        response.end(
+                            html.replace(/<script(?: type="text\/javascript")?/g, `$& nonce="${this.nonceFactory(request, response)}"`)
+                        );
+                    } else {
+                        response.end(html);
+                    }
+                } catch (err) {
+                    next(err);
                 }
-            } catch (err) {
-                next(err);
-            }
-        });
-    };
+            });
+    }
 
     getAngular(req: express.Request, res: express.Response, next: express.NextFunction) {
         if (this.checkSkip(req)) {
@@ -91,8 +108,7 @@ export class AngularController<T extends IAngularAppOptions = IAngularAppOptions
     }
 }
 
-
-function loadBundle(src: string): { Module: Type<any>, renderModule: typeof renderModule } {
+function loadBundle(src: string): { Module: Type<any>; renderModule: typeof renderModule } {
     const bundle = require(path.resolve(src));
 
     const moduleKey = Object.keys(bundle).find(k => k.endsWith('Module'));
