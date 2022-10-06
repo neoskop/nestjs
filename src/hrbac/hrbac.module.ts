d4d1f0317a26bba42a370101e6aa11a06e06e2fe
Mark Wecke <wecke@neoskop.de>
2022-10-06 16:27:26 +0200

    fix(hrbac): fix provider visibility in hrbac module

--- src/hrbac/hrbac.module.ts
+++ src/hrbac/hrbac.module.ts
@@ -1,14 +1,17 @@
 import {
-    HRBAC, PermissionManager,
-    PermissionTransfer, ResourceManager, RoleManager,
-    StaticPermissionManager, StaticResourceManager, StaticRoleManager
+    HRBAC,
+    PermissionManager,
+    PermissionTransfer,
+    ResourceManager,
+    RoleManager,
+    StaticPermissionManager,
+    StaticResourceManager,
+    StaticRoleManager
 } from '@neoskop/hrbac';
 import { IResources, IRoles } from '@neoskop/hrbac/ng';
 import { DynamicModule, Module } from '@nestjs/common';
 import { Request } from 'express';
 import { AsyncOptions, createAsyncProviders } from '../utils/providers';
-
-
 
 export interface HRBACModuleOptions {
     resolveUserIdForRequest(request: Request): string | null;
@@ -40,47 +43,39 @@ function permissionManagerFactory(permissionManager: StaticPermissionManager, op
 
 export const HRBAC_OPTIONS = 'HRBAC:options';
 
-@Module({
-    providers: [
-        StaticRoleManager,
-        StaticResourceManager,
-        StaticPermissionManager,
-        { provide: RoleManager, useFactory: roleManagerFactory, inject: [StaticRoleManager, HRBAC_OPTIONS] },
-        { provide: ResourceManager, useFactory: resourceManagerFactory, inject: [StaticResourceManager, HRBAC_OPTIONS] },
-        { provide: PermissionManager, useFactory: permissionManagerFactory, inject: [StaticPermissionManager, HRBAC_OPTIONS] },
-        {
-            provide: HRBAC, useFactory(roleManager: RoleManager, resourceManager: ResourceManager, permissionManager: PermissionManager) {
-                return new HRBAC(roleManager, resourceManager, permissionManager);
-            }, inject: [RoleManager, ResourceManager, PermissionManager]
-        }
-    ],
-    exports: [
-        HRBAC,
-        RoleManager,
-        PermissionManager
-    ]
-})
+const PROVIDERS = [
+    StaticRoleManager,
+    StaticResourceManager,
+    StaticPermissionManager,
+    { provide: RoleManager, useFactory: roleManagerFactory, inject: [StaticRoleManager, HRBAC_OPTIONS] },
+    { provide: ResourceManager, useFactory: resourceManagerFactory, inject: [StaticResourceManager, HRBAC_OPTIONS] },
+    { provide: PermissionManager, useFactory: permissionManagerFactory, inject: [StaticPermissionManager, HRBAC_OPTIONS] },
+    {
+        provide: HRBAC,
+        useFactory(roleManager: RoleManager, resourceManager: ResourceManager, permissionManager: PermissionManager) {
+            return new HRBAC(roleManager, resourceManager, permissionManager);
+        },
+        inject: [RoleManager, ResourceManager, PermissionManager]
+    }
+];
+const EXPORTS = [HRBAC, RoleManager, PermissionManager, HRBAC_OPTIONS];
+
+@Module({})
 export class HrbacModule {
     static forRoot(options: HRBACModuleOptions): DynamicModule {
         return {
             module: HrbacModule,
-            providers: [
-                { provide: HRBAC_OPTIONS, useValue: options }
-            ], exports: [
-                HRBAC_OPTIONS
-            ]
-        }
+            providers: [...PROVIDERS, { provide: HRBAC_OPTIONS, useValue: options }],
+            exports: EXPORTS
+        };
     }
 
     static forRootAsync(options: AsyncOptions<HRBACModuleOptions>): DynamicModule {
         return {
             module: HrbacModule,
             imports: options.imports,
-            providers: [
-                ...createAsyncProviders(options, HRBAC_OPTIONS)
-            ], exports: [
-                HRBAC_OPTIONS
-            ]
-        }
+            providers: [...PROVIDERS, ...createAsyncProviders(options, HRBAC_OPTIONS)],
+            exports: EXPORTS
+        };
     }
 }
